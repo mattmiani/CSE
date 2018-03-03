ca79c07f13ca1d91da9805b54bedcf26bc7c61a2
Matt Miani <mattmiani@gmail.com>
2018-03-02 17:40:01 -0800

    Update DNA.java

--- DNA.java
+++ DNA.java
@@ -1,30 +1,52 @@
-
+//Matt Miani
+//CSE 142
+//TA: Aidan Hunt
+// 2/27/18
+// This program takes in a file containing nucleotide sequences and region
+// identifiers, and creates an output file with certain information about
+// those sequences; mass percentages of ACTG, list of codons, etc.
 
 import java.util.*;
 import java.io.*;
 
 public class DNA {
+   //class constants for minimum number of codons of a valid protein,
+   //minimum combined percentage of cytosine and guanine for a valid protein
+   //length of a codon
+   //number of unique bases
+   
    static int MIN_CODONS = 5;
    static double CG_PERCENT= 30.0;
    static int CODON_LENGTH =3;
-   
+   static int BASES = 4; 
    
    public static void main (String [] args)
    throws FileNotFoundException {
       
       Scanner user = new Scanner(System.in);
       
-     File input = prompt(user);
-     scan(input); 
-      //scan 
-      //nucleotides
-      //cgPercent
-      //codon list
-      //check protein 
+      File input = inputPrompt(user); //input prompt
+      File output = outputPrompt(user); //output prompt
+      PrintStream result = new PrintStream(output);
+      
+      Scanner fileScan = new Scanner(input);
+      while (fileScan.hasNextLine()) {
+         String line = fileScan.nextLine();
+         
+         if (line.contains(" ")) {
+            result.println("Region Name: " + line);
+         } else {
+            scan(line, result);
+            nucCount(line, result);
+            double minPercent= calcPercent(line.toLowerCase(), result);
+            String[] codons = codonSet(line, result);
+            checkProtein(codons, result, minPercent);
+         }
+      }
    }
    
-   
-   public static File prompt(Scanner user)
+   //user prompt for input file
+   public static File inputPrompt(Scanner user)
    throws FileNotFoundException {
       System.out.println("This program reports information about DNA");
       System.out.println("nucleotide sequences that may encode proteins.");
@@ -35,56 +57,35 @@ public class DNA {
       while(!input.exists()) {
          System.out.print("File not found. ");
          input = new File(user.nextLine()) ;
-      }
+      } 
+      return input;    
+   }
+   //user prompt for output file
+   public static File outputPrompt(Scanner user)
+   throws FileNotFoundException {
+      System.out.print("Output file name? ");
       
-      
-      //scan(input);
-      return input; 
-      
+      File output = new File(user.nextLine());      
+      return output;  
    }
    
-   //prompt needs to feed input file to here
-   public static void scan(File in)
+   //goes through input file and finds nucleotide sequences to print
+   public static void scan(String line, PrintStream result)
    throws FileNotFoundException {
       
-      Scanner fileScan = new Scanner(in);
-      Scanner user = new Scanner (System.in);
+      Scanner lineScan = new Scanner(line);
+      String sequence = line.toUpperCase();
       
-      System.out.print("Output file name? ");
-      File out = new File (user.nextLine());
-      PrintStream result = new PrintStream(out);
-      
-      
-      
-      while (fileScan.hasNextLine()) {
-         String line = fileScan.nextLine();
-         if (!line.contains(" ")){
-            report(line, result);
-            
-         } else {
-            //print region name to output
-            result.println("Region Name: " + line);
-            
-         }
-      }
-      
-      
-      
+      result.println("Nucleotides: " + sequence);
    }
-      public static void report(String input, PrintStream result)
+   
+   //finds the number of each nucleotide base
+   public static void nucCount(String input, PrintStream result)
    throws FileNotFoundException {
-      
-      int[] total = new int[4];
-      
-      
+      int[] total = new int[BASES];
       String line = input.toUpperCase();
       String nuc = "ACGT";
-      
-      
-      result.println("Nucleotides: " + line);
-      ;
       for (int i = 0; i<line.length(); i++) {
-         
          char c = line.charAt(i);
          for (int j = 0; j<=CODON_LENGTH; j++) {
             if (c == nuc.charAt(j)) {
@@ -93,21 +94,24 @@ public class DNA {
          }
       }
       result.println("Nuc. Counts: " + Arrays.toString(total));
-      double cgPercent = calcPercent(input.toLowerCase(), result);
-      
-      String codons = input.toUpperCase().replace("-","");
-      
-      String[] codonSet = new String[(codons.length()/CODON_LENGTH)];
-      
-      for (int i = 0; i<= (codons.length()/CODON_LENGTH)-1; i++) {
-         codonSet[i] = codons.substring(CODON_LENGTH*i, CODON_LENGTH*i+CODON_LENGTH);
-      }
-      
-      result.println("Codons List: " + Arrays.toString(codonSet));
-      checkProtein(codonSet, result, cgPercent);
-      
    }
    
+   
+   //separates nucleotide sequence into codons, returns as an array
+   public static String[] codonSet(String input, PrintStream result)
+   throws FileNotFoundException {
+      String codons = input.toUpperCase().replace("-","");
+      String[] codonSet = new String[ (codons.length()/CODON_LENGTH)];
+      
+      for (int i=0; i<=(codons.length()/CODON_LENGTH)-1; i++) {
+         codonSet[i] = codons.substring(CODON_LENGTH*i, CODON_LENGTH*i+CODON_LENGTH);
+      }
+      result.println("Codons List: " + Arrays.toString(codonSet));
+      return codonSet;
+   }
+   
+   //calculates the mass percentage of each nucleotide base in the sequence
+   //returns the combined percentage of cytosine and guanine
    public static double calcPercent(String line, PrintStream result)
    throws FileNotFoundException {
       double adenine = 135.128;
@@ -124,18 +128,12 @@ public class DNA {
       massSet[4] = junk;
       
       double[] rawMass = new double[5];
-      //process by character
-      //add to total sum \
-      //add up amount of each
-      //end with mass rawMassages
-      
-      //need array with nuc counts for easier math
       String nuc = "acgt-";
       
       for (int i = 0; i<line.length(); i++) {
          char c = line.charAt(i);
          
-         for (int j = 0;j<=4; j++){
+         for (int j = 0;j<=BASES; j++){
             
             if (c==nuc.charAt(j)) {
                
@@ -150,10 +148,9 @@ public class DNA {
       }
       sum = Math.round(sum*10.0) / 10.0;
       
-      double[] percent = new double[4];
+      double[] percent = new double[BASES];
       for (int i = 0; i<percent.length; i++) {
          percent [i] = Math.round( (rawMass[i]/sum)*1000.0) /10.0;
-         // percent[i] = (rawMass[i]/sum) * 100;
       }
       
       result.println("Total Mass%: " + Arrays.toString(percent) + " of " + sum);
@@ -161,11 +158,11 @@ public class DNA {
       double cgPercent = percent[1] + percent[2];
       return cgPercent;
    }
+   
+   //uses the conditions in the spec to predict if the sequence could be a protein
    public static void checkProtein(String[] codonSet, PrintStream result, double cgPercent)
    throws FileNotFoundException {
       
-      
-      //string array traversal for ending codon
       String[] stop = new String[3];
       stop[0] = "TAG";
       stop[1] = "TAA";
@@ -197,6 +194,4 @@ public class DNA {
       }
       result.println();
    }
-   
-   
 }
